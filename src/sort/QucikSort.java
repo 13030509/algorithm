a836254944a42b93e9d54b27f7f6a89008874541
13030509 <597349771@qq.com>
2019-04-23 15:54:25 +0800

    提交测试

--- src/sort/QucikSort.java
+++ src/sort/QucikSort.java
@@ -10,12 +10,17 @@ public class QucikSort {
         quickSort(arr, 0, end);
         ArrayUtil.printArr(arr);
 
+        int[] arr1 = {5, 2, 12, 52, 78, 2, 3, 1, 4, 0};
+        findKElement(arr,5,0,arr1.length-1);
+
+
     }
 
 
 
     public static void quickSort(int[] arr, int low, int high) {
 
+        //快速排序如果每次选择的点都不好 ， 时间复杂度会退化为O(n2) 优化方法 1 随机取 2 三数取中
         if (low >= high) {
             return;
         }
@@ -25,6 +30,27 @@ public class QucikSort {
         quickSort(arr, low, mid-1);
         quickSort(arr, mid+1, high);
 
+
+    }
+
+    //查找第k大的元素 要求时间复杂度为O(n)
+
+    public static void findKElement(int[] arr , int k ,int low, int high){
+
+
+        int mid = partition(arr, low, high);
+
+        if (mid +1 == k) {
+            System.out.println("第" + k  + "大的元素为" + arr[mid]);
+            return;
+        }
+
+        if(mid + 1 >k){
+            findKElement(arr, k,low, mid-1);
+        }else{
+            findKElement(arr, k,mid+1, high);
+
+        }
 
     }
 
